1e5c82507a0762fb057407151732f3832ab5e316
MaximkaCoder <99674975+MaximkaCoder@users.noreply.github.com>
2023-12-18 02:10:15 +0200

    success version captcha solve

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import undetected_chromedriver as uc
+
+import captcha
 from solve_captcha import solveRecaptcha
 from captcha import send_solve
 from time import sleep
@@ -16,9 +18,14 @@ import multiprocessing
 
 
 def registration(phone_num, position):
-    chrome_options = uc.ChromeOptions()
-    chrome_options.add_argument("--use_subprocess")
-    browser = uc.Chrome(options=chrome_options, driver_executable_path="D:\\temp\\chromedriver.exe")
+    options = uc.ChromeOptions()
+    options.add_argument(r'--load-extension=D:\Python_projects\amazon_reg\Captcha-Solver')
+    options.add_argument('--disable-popup-blocking')
+    options.add_argument('--no-first-run')
+    options.add_argument('--no-default-browser-check')
+    options.add_argument("--use_subprocess")
+    browser = uc.Chrome(options=options, driver_executable_path="D:\\temp\\chromedriver.exe")
+
     browser.get('https://accounts.google.com/')
     browser.set_window_position(*position)
     browser.set_window_size(400, 500)
@@ -32,62 +39,7 @@ def registration(phone_num, position):
         browser.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input').send_keys("Genius2003")
         browser.find_element(By.ID, "passwordNext").click()
         sleep(5)
-        browser.get("https://vulkanvegas.com/ru/register")
-        sleep(2)
-        list_button = browser.find_elements(by=By.XPATH, value="//li[contains(text(), 'Телефон')]")[0]
-        browser.execute_script("window.open('https://vulkanvegas.com/ru/register');")
-        browser.switch_to.window(browser.window_handles[-1])
-        browser.execute_script("arguments[0].scrollIntoView();", list_button)
-        list_button.click()
-        sleep(1)
-        browser.find_element(By.XPATH, '/html/body/div[4]/div/div/div[3]/form[2]/div[2]/div[1]/div/input').click()
-        browser.find_element(By.XPATH, '/html/body/div[4]/div/div/div[3]/form[2]/div[1]/div[1]/div[1]/label').click()
-        browser.find_element(By.XPATH, '/html/body/div[4]/div/div/div[3]/form[2]/div[1]/div[1]/div[1]/div/div/div[1]').click()
-        sleep(1)
-        set_country = browser.find_element(By.XPATH,
-                                           '//*[@id="popup-phone-sign-up"]/div[1]/div[1]/div[1]/div/div/div['
-                                           '2]/section/div[1]'
-                                           )
-        browser.execute_script("arguments[0].click();", set_country)
-
-        browser.find_element(By.XPATH,
-                             '/html/body/div[4]/div/div/div[3]/form[2]/div[1]/div[1]/div[1]/div/input'
-                             ).send_keys(phone_num)
-        browser.find_element(By.XPATH,
-                             '/html/body/div[4]/div/div/div[3]/form[2]/div[1]/div[2]/div[1]/div/input'
-                             ).send_keys("dsbjfbdsjhbdshj")
-        reg_butt = browser.find_element(By.XPATH, '/html/body/div[4]/div/div/div[3]/form[2]/button')
-        browser.execute_script("arguments[0].click();", reg_butt)
-
-        wait.until(EC.presence_of_element_located((By.ID, 'g-recaptcha-response')))
-
-        ######################################################
-
-        result = solveRecaptcha(
-            "6LfXp8kjAAAAACn75MdK75tol2FHjid-RNSlE0zr",
-            "https://vulkanvegas.com/ru/register"
-        )
-        #
-        code = result['code']
-        #
-        print(code)
-        #
-        element = (By.ID, 'g-recaptcha-response')
-        WebDriverWait(browser, 120).until(
-            EC.presence_of_element_located(element)
-        )
-        #
-        browser.execute_script("document.getElementById('g-recaptcha-response').innerHTML = " + "'" + code + "'")
-        # sleep(5)
-        ######################################################
-        sleep(5)
-        target_div = browser.find_element(By.XPATH,
-                                          "/html/body/div[4]/div/div/div[3]/form[2]/div[4]/div/div[1]")
-        new_input = browser.execute_script('return arguments[0].appendChild(document.createElement("input"))',
-                                           target_div)
-        browser.execute_script('arguments[0].setAttribute("type", "submit")', new_input)
-        browser.execute_script('arguments[0].setAttribute("value", "send")', new_input)
-        new_input.send_keys(Keys.RETURN)
+        captcha.send_solve(browser, phone_num)
         sleep(60)
         browser.quit()
     except Exception:
@@ -111,7 +63,7 @@ def run_registration(position):
 
 
 if __name__ == '__main__':
-    count_of_window = 1
+    count_of_window = 2
     positions = [(0, 0), (0, 500), (550, 0), (550, 500)]
 
     for i in range(count_of_window):
